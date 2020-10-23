5c5ee6bcecbd38fc27025e70e52c37cd8e0ceaf5
sammylt <73323893+sammylt@users.noreply.github.com>
2020-10-22 22:54:16 -0400

    Add files via upload

---
+++ init.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    var canvas = document.getElementsByTagName("canvas")
+    for (var i = 0; i < canvas.length; i++) {
+        Processing(canvas[i], canvas[i].previousSibling.textContent);
+    }
+};
